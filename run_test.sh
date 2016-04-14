ad620344a685322a00c6b3612624fb267910ba6f
Yun Huang <huangyunict@gmail.com>
2016-04-13 22:58:05 -0500

    add test script

--- run_test.sh
+++ run_test.sh
@@ -7,13 +7,14 @@ if [ -e "${text_file}" ]
 then
     rm "${text_file}"
 fi
-#echo "你好！世界！" >> "${text_file}"
-#echo "一二三四五六七八九十" >> "${text_file}"
-#echo "睡觉是第四声，觉得是第三声" >> "${text_file}"
-#echo "难得糊涂，空难事故" >> "${text_file}"
+echo "你好！世界！" >> "${text_file}"
+echo "一二三四五六七八九十" >> "${text_file}"
 echo "背上 背着 背包" >> "${text_file}"
+echo "睡觉是第四声，觉得是第三声" >> "${text_file}"
+echo "难得糊涂，空难事故" >> "${text_file}"
 
 #   test
+#./ekho -v "Cantonese" -f "${text_file}"
 ./ekho -v "Mandarin" -f "${text_file}"
 
 #   clean
