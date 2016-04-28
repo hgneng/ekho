e23af524045df906376010381586b13f0362c056
Yun Huang <huangyunict@gmail.com>
2016-04-27 21:53:13 -0500

    add ekho_typedef

---
+++ src/ekho_typedef.h
@@ -0,0 +1,22 @@
+#ifndef EKHO_TYPEDEF_H
+#define EKHO_TYPEDEF_H
+
+namespace ekho {
+
+enum Command {
+    SPEAK,
+    SAVEMP3,
+    SAVEOGG,
+    GETPHONSYMBOLS
+};
+
+enum EkhoPuncType {
+    EKHO_PUNC_NONE = 1,
+    EKHO_PUNC_SOME,
+    EKHO_PUNC_ALL
+};
+
+}
+
+#endif
+
