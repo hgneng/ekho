c9e70a7f7eb4a9531f6670d2e5006f10eee1c13d
Cameron Wong <hgneng@gmail.com>
2016-06-17 11:29:45 +0800

    add sapi5 deps

---
+++ sapi5/include/speech_tools/win32/EST_socket_win32.h
@@ -0,0 +1,63 @@
+
+
+ /************************************************************************/
+ /*                                                                      */
+ /*                Centre for Speech Technology Research                 */
+ /*                     University of Edinburgh, UK                      */
+ /*                       Copyright (c) 1996,1997                        */
+ /*                        All Rights Reserved.                          */
+ /*                                                                      */
+ /*  Permission is hereby granted, free of charge, to use and distribute */
+ /*  this software and its documentation without restriction, including  */
+ /*  without limitation the rights to use, copy, modify, merge, publish, */
+ /*  distribute, sublicense, and/or sell copies of this work, and to     */
+ /*  permit persons to whom this work is furnished to do so, subject to  */
+ /*  the following conditions:                                           */
+ /*   1. The code must retain the above copyright notice, this list of   */
+ /*      conditions and the following disclaimer.                        */
+ /*   2. Any modifications must be clearly marked as such.               */
+ /*   3. Original authors' names are not deleted.                        */
+ /*   4. The authors' names are not used to endorse or promote products  */
+ /*      derived from this software without specific prior written       */
+ /*      permission.                                                     */
+ /*                                                                      */
+ /*  THE UNIVERSITY OF EDINBURGH AND THE CONTRIBUTORS TO THIS WORK       */
+ /*  DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING     */
+ /*  ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT  */
+ /*  SHALL THE UNIVERSITY OF EDINBURGH NOR THE CONTRIBUTORS BE LIABLE    */
+ /*  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   */
+ /*  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN  */
+ /*  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,         */
+ /*  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF      */
+ /*  THIS SOFTWARE.                                                      */
+ /*                                                                      */
+ /*************************************************************************/
+ /*                                                                       */
+ /*                 Author: Richard Caley (rjc@cstr.ed.ac.uk)             */
+ /*                   Date: Tue Sep4th 1997                               */
+ /* --------------------------------------------------------------------  */
+ /* Defines to make Winsock more like unix sockets.                       */
+ /*                                                                       */
+ /*************************************************************************/
+
+#if !defined(__EST_SOCKET_WIN32_H__)
+#define __EST_SOCKET_WIN32_H__ 1
+
+#define SOCKLEN_CAST (int *)
+
+typedef int ssize_t;
+typedef int socklen_t;
+#define NOT_A_SOCKET(FD) ((FD) == INVALID_SOCKET)
+#define socket_error() WSAGetLastError()
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+  int socket_initialise(void);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
