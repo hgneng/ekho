aab0a89744f7c4e7a5c25c74f1437bb3d30d0605
Cameron Wong <hgneng@gmail.com>
2018-03-29 16:29:24 +0800

    add script

---
+++ src/make_musicxml.sh
@@ -0,0 +1,1 @@
+gcc musicxml.cpp -I../libmusicxml/src/elements -I../libmusicxml/src/lib -I../libmusicxml/src/visitors -I../libmusicxml/src/files -I../libmusicxml/src/parser ../libmusicxml/libmusicxml2.a -lstdc++
